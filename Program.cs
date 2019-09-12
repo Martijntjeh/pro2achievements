3500bc3041ae425e8a2e2388955c71dd461ee741
Martijntjeh <54798853+Martijntjeh@users.noreply.github.com>
2019-09-12 12:09:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            Console.WriteLine("############################################");
+            Console.WriteLine("############WEET IK VEEL####################");
+            Console.WriteLine("############################################");
+            Console.WriteLine("############################################");
+            
+            Console.Readline();
+        }
+    }
+}
